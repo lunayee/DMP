980f5e58877576df82702ed195a59ebf385f4b89
Luyee <abc5542211@gmail.com>
2022-05-27 13:33:41 +0800

    資料庫嫁接網頁成功

--- core/settings.py
+++ core/settings.py
@@ -130,6 +130,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = (
+    os.path.join(CORE_DIR, 'apps/static'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
